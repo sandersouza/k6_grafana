b2af83bdf8e3e622732ed32c2bcd944462d76984
sandersouza <sander@unixwork.com.br>
2024-04-22 11:10:33 -0300

    update script

--- scripts/example.js
+++ scripts/example.js
@@ -1,17 +1,46 @@
 import http from 'k6/http';
-import { sleep, check } from 'k6';
+import { sleep, check, group } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '1m', target: 20 },
+        { duration: '1m', target: 25 },
+        { duration: '1m', target: 30 },
+        { duration: '1m', target: 35 },
+        { duration: '1m', target: 40 },
+        { duration: '1m', target: 45 },
+        { duration: '1m', target: 50 },
     ],
+    tags: {
+        env: "production"
+    }
 };
 
 export default function () {
-    let res = http.get('http://test.k6.io');
-    check(res, {
-        "is status 200": (r) => r.status === 200,
+    group('Test k6.io', function() {
+        let paramsK6 = {
+            tags: {
+                testid: "k6-io-test"
+            }
+        };
+        let resK6 = http.get('http://test.k6.io', paramsK6);
+        check(resK6, {
+            "status is 200": (r) => r.status === 200,
+            "transaction time OK": (r) => r.timings.duration < 200
+        });
     });
-    console.log(`Duration: ${res.timings.duration}`);
+
+    group('Test Grafana', function() {
+        let paramsGrafana = {
+            tags: {
+                testid: "grafana-test"
+            }
+        };
+        let resGrafana = http.get('http://grafana:3000', paramsGrafana);
+        check(resGrafana, {
+            "status is 200": (r) => r.status === 200,
+            "transaction time OK": (r) => r.timings.duration < 200
+        });
+    });
+
     sleep(1);
 }
