50e4dca4dfbb208f37ac934b422f4eea89ebc921
sandersouza <sander@unixwork.com.br>
2024-04-22 01:10:41 -0300

    Change InfluxDB by Prometheus, and another adjusts

--- scripts/example.js
+++ scripts/example.js
@@ -1,13 +1,17 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '1m', target: 20 }, // Simula 20 usuários por 1 minuto
+        { duration: '1m', target: 20 },
     ],
 };
 
 export default function () {
-    http.get('http://test.k6.io');
+    let res = http.get('http://test.k6.io');
+    check(res, {
+        "is status 200": (r) => r.status === 200,
+    });
+    console.log(`Duration: ${res.timings.duration}`);
     sleep(1);
 }
